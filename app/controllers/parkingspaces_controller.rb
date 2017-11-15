58b6d87920cda5843df05d1b307c96560f8eb438
Joanna Tracy <joanna98146@yahoo.com>
2017-11-15 21:03:12 +0000

    ref point

--- app/controllers/parkingspaces_controller.rb
+++ app/controllers/parkingspaces_controller.rb
@@ -1,4 +1,21 @@
 class ParkingspacesController < ApplicationController
     def index
+        @parkingspaces = Parkingspaces.all
+    end
+  
+  def new
+      @parkingspaces = Parkingspaces.new
   end
+ 
+  def create
+    Parkingspaces.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def parkingspace_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+  
 end
